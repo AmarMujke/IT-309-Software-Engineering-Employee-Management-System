b3028963adc577b4eb50575f265211e7f1777899
vedran-kilalic <vedrankilalic@gmail.com>
2023-04-01 13:51:31 +0200

    Attendance and payroll model

---
+++ src/main/java/com/employee/management/model/Attendance.java
@@ -0,0 +1,77 @@
+package com.employee.management.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+@Entity
+@Table(name = "attendance")
+public class Attendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "attendance_id")
+    private long id;
+
+    @Column(name = "employee_id" )
+    private int employee_id;
+
+    @Column(name = "date" )
+    private LocalDate date;
+
+    @Column(name = "time_in" )
+    private LocalTime time_in;
+
+    @Column(name = "time_out" )
+    private LocalTime time_out;
+    @Column(name = "status")
+    private String status;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(int employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime_in() {
+        return time_in;
+    }
+
+    public void setTime_in(LocalTime time_in) {
+        this.time_in = time_in;
+    }
+
+    public LocalTime getTime_out() {
+        return time_out;
+    }
+
+    public void setTime_out(LocalTime time_out) {
+        this.time_out = time_out;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
